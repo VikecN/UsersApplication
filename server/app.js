59c07209ccf264b1837f1b86db95c55de67e709d
VikecN <viktornedanovski31@gmail.com>
2022-05-09 00:39:29 +0200

    Database Connection & Cookie Parser

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
+require("./configuration/database").connect();
 const express = require('express');
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
+
+const { API_PORT } = process.env;
 
 const app = express();
 
 //Middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 //Paths
 const auth = require('./routes/auth/auth.js');
@@ -18,5 +23,4 @@ app.use("/authentication", auth);
 app.use("/", home);
 app.use("/admin", admin);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log("Server started at PORT: " + PORT));
+app.listen(API_PORT, console.log(`Server started on PORT: ${API_PORT}`))
